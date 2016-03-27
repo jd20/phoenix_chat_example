ec31a55fc09de7bbe8432ca39ed7d044fc83b7fb
Jason Douglas <jasond@me.com>
2016-03-26 19:37:34 -0700

    Increase pool size to 40

--- config/config.exs
+++ config/config.exs
@@ -12,7 +12,8 @@ config :chat, Chat.Endpoint,
   secret_key_base: "/RjKJmMO6raXPRTq63qTqid1x6lVKTOP+FTxZHfX6Ogd+1xYmH6eZZFhBu1CIwtg",
   debug_errors: false,
   pubsub: [name: Chat.PubSub,
-           adapter: Phoenix.PubSub.PG2]
+           adapter: Phoenix.PubSub.PG2,
+           pool_size: 40]
 
 # Configures Elixir's Logger
 config :logger, :console,
